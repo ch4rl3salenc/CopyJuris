851560c4f52a7c4cce3a6a0ffbf6ad77892d0d72
ch4rl3salenc <ch4rl3s@gmail.com>
2020-07-28 15:11:01 -0300

    Update Copiar Jurisprudência Jusbrasil.user.js

--- Copiar Jurisprudência Jusbrasil.user.js
+++ Copiar Jurisprudência Jusbrasil.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name          Copiar Jurisprudência Jusbrasil
-// @namespace     Copiar
-// @description   copia
-// @shortcutKeys  [Ctrl + `] ativação
-// @author        Hackerman
-// @version       0.1
-// @include       http*://*.jusbrasil.com.br/*
-// @icon          asdf
-// @compatible    Chrome Google Chrome + Tampermonkey
-// @grant         GM_registerMenuCommand
-// @license       BSD
-// @copyright     Hackerman, 02-07-2020
+// @name                    Copiar Jurisprudência Jusbrasil
+// @namespace               Copiar
+// @description             copia
+// @shortcutKeys            [Ctrl + `] ativação
+// @license License Type    MIT
+// @author                  ch4rl3salenc
+// @version                 0.1
+// @include                 http*://*.jusbrasil.com.br/*
+// @icon                    asdf
+// @compatible              Chrome Google Chrome + Tampermonkey
+// @grant                   GM_registerMenuCommand
+// @license                 BSD
+// @copyright               ch4rl3salenc, 02-07-2020
 // ==/UserScript==
 
 (function() {
@@ -29,11 +30,8 @@
             document.body.appendChild(textArea);
             textArea.select();
             document.execCommand("Copy");
-
-
     }
 
 }, 1000)
-
 
 })();
